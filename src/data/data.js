77906b810b98accbb74ed569dc1692eacf83dfba
LeonWu813 <leonwuya@gamil.com>
2023-02-05 21:56:32 +0800

    Add Space

--- src/data/data.js
+++ src/data/data.js
@@ -3,8 +3,7 @@ export default [
     id: 1,
     title: "GoFreight Website",
     description: [
-      "案例為GoFreight官方網站改版，作為一款功能多樣強大的Saas軟體，網站將會著重如何讓複雜資訊輕易讓消費者理解進而對產品產生興趣與信賴感。",
-      "GoFreight專為貨運承攬 (Freight Forwarder) 與物流產業打造的國際物流科技平台，透過工作流程自動化集中所有業務操作與數據，協助國際物流業者將顧客關係管理、行銷、會計、貨運流程與商業分析等資訊數位化，亦加速物流產業的數位化轉型。",
+      "案例為 GoFreight 官方網站改版，作為一款功能多樣強大的 SaaS 軟體，網站將會著重如何讓複雜資訊輕易讓消費者理解進而對產品產生興趣與信賴感。",
     ],
     coverImg: "/portfolio/images/GoFreight/HP_pic.png",
     tag: {
@@ -17,7 +16,7 @@ export default [
     id: 2,
     title: "GoFreight Social Media",
     description: [
-      "於GoFreight擔任Marketing Design Intern，負責Social Media社群貼文創意設計以及GoFreight Pay Tutorials動畫製作。",
+      "於 GoFreight 擔任 Marketing Design Intern，負責 Social Media 社群貼文創意設計以及 GoFreight Pay Tutorials 動畫製作。",
     ],
     coverImg: "/portfolio/images/Social/HP_pic.png",
     tag: {
@@ -30,8 +29,7 @@ export default [
     id: 3,
     title: "Workfe",
     description: [
-      "Workfe是一款希望讓使用者找到中意的咖啡廳，同時增加小眾咖啡廳的曝光。 主要的工具為滿座率，為解決“人已經到了咖啡廳，才發現沒有位置可以享受”的現象。",
-      "結合地圖和咖啡同好，創造出史上最完整的咖啡廳app，建立一個充滿咖啡香的社群！",
+      "Workfe 是一款希望讓使用者找到中意的咖啡廳，同時增加小眾咖啡廳的曝光。主要的工具為滿座率，為解決“人已經到了咖啡廳，才發現沒有位置可以享受”的現象。結合地圖和咖啡同好，創造出史上最完整的咖啡廳app，建立一個充滿咖啡香的社群！",
     ],
     coverImg: "/portfolio/images/Workfe/HP_pic.png",
     tag: {
@@ -44,8 +42,7 @@ export default [
     id: 4,
     title: "Foodie Snail",
     description: [
-      "從IDEO線上課程學習運用設計思考的方法解決課題，本次課題為：How Might We Provide Healthier Food Options for People in Need?",
-      "透過訪談調研來了解在乎健康、租屋上班族的生活經驗與習慣，找到相關痛點，結合食譜以及熱量紀錄系統，以APP的形式提供更健康食物的選擇。",
+      "參與 IDEO 課題目標為：How Might We Provide Healthier Food Options for People in Need? 透過訪談調研來了解在乎健康、租屋上班族的生活經驗與習慣，找到相關痛點，結合食譜以及熱量紀錄系統，以 APP 的形式提供更健康食物的選擇。",
     ],
     coverImg: "/portfolio/images/Foodie_snail/HP_pic.png",
     tag: {
@@ -58,8 +55,7 @@ export default [
     id: 5,
     title: "長者健康管理平台",
     description: [
-      "長者健康管理平台主要為協助國健署輔導網路團隊、地方政府以及社區有效管理及掌握個案參與課程與社團的情形。",
-      "藉由前、後測數值追蹤在課堂內學員的基本資料，提供簡易的評估報告書提高對長者健康狀況的掌握。",
+      "長者健康管理平台主要為協助國健署輔導網路團隊、地方政府以及社區有效管理及掌握個案參與課程與社團的情形。藉由前、後測數值追蹤在課堂內學員的基本資料，提供簡易的評估報告書提高對長者健康狀況的掌握。",
     ],
     coverImg: "/portfolio/images/HPAhealth/HP_pic.png",
     tag: {
@@ -72,8 +68,7 @@ export default [
     id: 6,
     title: "Ease",
     description: [
-      "整合GIS地理資訊系統所需要的工具：集蒐集、儲存、處理、分析、展示和查詢地理空間資料，並提供組織及參與團隊開發功能，輕易地融入地理資料發展、計畫和資訊等交流。",
-      "任何人都能輕易地瀏覽、分析及展示GIS地圖。探索視覺化的資料，使大眾更容易地觀察、理解並解釋地理資訊空間分布的關係。幫助做出更好的決策，進而規劃出更合適的遠景。",
+      "EASE 希望打造任何人都能輕易地瀏覽、分析及展示 GIS 地圖。探索視覺化的資料，使大眾更容易地觀察、理解並解釋地理資訊空間分布的關係。幫助做出更好的決策，進而規劃出更合適的遠景。",
     ],
     coverImg: "/portfolio/images/ease/HP_pic.png",
     tag: {
